ea1f63f3351b5cdc9333d3cfe8505389580bd4d8
Yuta Aoki <nightrage.metal66@gmail.com>
2021-10-27 20:53:10 +0900

    Imported the decorated_icon library

--- lib/import.dart
+++ lib/import.dart
@@ -2,3 +2,4 @@ export 'package:flutter/material.dart';
 export 'package:flutter/widgets.dart';
 export 'package:flutter_form_builder/flutter_form_builder.dart';
 export 'widgets/styles/header_styles.dart';
+export 'package:decorated_icon/decorated_icon.dart';
