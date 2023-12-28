ce142257b72684e04cafb803a21f4aa12c708496
uuta <nightrage.metal66@gmail.com>
2023-12-28 23:36:42 +0900

    Remove duplicate import statement for location state

--- lib/models/states/states.dart
+++ lib/models/states/states.dart
@@ -7,6 +7,5 @@ export '/models/states/purchase/purchase_state_notifier.dart';
 // state
 export '/models/states/auth0/auth0_state.dart';
 export '/models/states/location/location_state.dart';
-export '/models/states/location/location_state.dart';
 export '/models/states/navigation/navigation_state.dart';
 export '/models/states/purchase/purchase_state.dart';
